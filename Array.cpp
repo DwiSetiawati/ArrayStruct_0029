2c781001634cc8c7a26ac1dfb1c97248036ae0fc
DwiSetiawati <dwisetwa27@gmail.com>
2025-04-14 10:53:05 +0700

    Mendeklarasikan array dengan ukuran dan tanpa nilai awal

--- Array.cpp
+++ Array.cpp
@@ -6,3 +6,6 @@ int nilai[] = {1, 2, 3};
 
 // mendeklarasikan array dengan ukuran dan diberi nilai awal
 string nama[5] = {"ben", "agus", "fara", "ama", "akila"};
+
+// mendeklarasikan array dengan ukuran dan tanpa nilai awal
+float score[5];
