4796bb21e126b33aa2cac4f737caebca52122281
DwiSetiawati <dwisetwa27@gmail.com>
2025-04-14 11:04:59 +0700

    Menampilkan score

--- Array.cpp
+++ Array.cpp
@@ -9,3 +9,30 @@ string nama[5] = {"ben", "agus", "fara", "ama", "akila"};
 
 // mendeklarasikan array dengan ukuran dan tanpa nilai awal
 float score[5];
+
+int main()
+{
+    cout << "Nama pada index 4 adalah " << nama[3] << endl;
+    // memberi nilai pada elemen array per index
+    nama[4] = "Tia";
+    cout << "Nama pada index 4 adalah " << nama[3] << endl;
+
+    cout << "inputscore" << endl;
+    cout << endl;
+
+    // memberi nilai array dengan looping for
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "score ke-" << i + 1 << "=";
+        cin >> score[i];
+    }
+
+    cout << "Tampilkan score" << endl;
+    cout << endl;
+
+    // memberi nilai array dengan looping for
+    for (int i = 0; 1 < 5; i++)
+    {
+        cout << "Score ke-" << i + 1 << "=" << score[i] << endl;
+    }
+}
